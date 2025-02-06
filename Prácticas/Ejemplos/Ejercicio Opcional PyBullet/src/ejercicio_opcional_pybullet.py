98632c970450d37aae3b80c7dd1b63406ef16f82
aleon2020 <a.leon.2020@alumnos.urjc.es>
2025-02-06 17:09:15 +0100

    UPLOADING FILES ...

--- Prácticas/Ejemplos/Ejercicio Opcional PyBullet/src/ejercicio_opcional_pybullet.py
+++ Prácticas/Ejemplos/Ejercicio Opcional PyBullet/src/ejercicio_opcional_pybullet.py
@@ -1,4 +1,4 @@
-# Imports de las librerías
+# Imports de las librerías.
 import pybullet as p
 import pybullet_data
 import argparse
@@ -7,42 +7,41 @@ import time
 parser = argparse.ArgumentParser(description="URDF viewer example")
 parser.add_argument("--urdf", type=str, required=True, help="Ruta al archivo URDF.")
 args = parser.parse_args()
-urdf_path = "urdf/ejercicio_opcional_pybullet.urdf"
 
-# Conectamos motor con GUI
-physicsClient = p.connect (p.GUI)
-p.setAdditionalSearchPath (pybullet_data.getDataPath())
+# Conectamos motor con GUI.
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-# Establecemos gravedad (X,Y,Z)
-p.setGravity (0, 0, -9.8)
+# Establecemos gravedad (X,Y,Z).
+p.setGravity(0, 0, -9.8)
 
-# Cargamos un/unos modelo/s
-planeId = p.loadURDF ("plane.urdf")
+# Cargamos un/unos modelo/s.
+planeId = p.loadURDF("plane.urdf")
 
-startPosition = [0, 0, 1]
+startPosition = [0, 0, 0]
 startOrientation = p.getQuaternionFromEuler([0, 0, 0])
 
 # Cargamos un nuevo objeto, con una posición (x,y,z)
-# y una orientación dada en cuaternión (X,Y,Z)
-robotId = p.loadURDF (urdf_path, startPosition, startOrientation)
+# y una orientación dada en cuaternión (X,Y,Z).
+robotId = p.loadURDF("urdf/ejercicio_opcional_pybullet.urdf", startPosition, startOrientation)
 
-frictionIdBase = p.addUserDebugParameter("BASE_jointFriction", 0, 100, 10)
-torqueIdBase = p.addUserDebugParameter("BASE_joint torque", -20, 20, -9)
-frictionId = p.addUserDebugParameter("ARM_jointFriction", 0, 100, 10)
-torqueId = p.addUserDebugParameter("ARM_joint torque", -20, 20, -9)
+frictionIdBase = p.addUserDebugParameter("jointFrictionBase", 0, 100, 10)
+torqueIdBase = p.addUserDebugParameter("jointTorqueBase", -20, 20, -9)
+frictionIdTop = p.addUserDebugParameter("jointFrictionTop", 0, 100, 10)
+torqueIdTop = p.addUserDebugParameter("jointTorqueTop", -20, 20, -9)
 
 numJoints = p.getNumJoints(robotId)
 print("NumJoints: " + str(numJoints))
 
 for j in range (numJoints):
-    print("%d - %s" % (p.getJointInfo(robotId,j)[0], p.getJointInfo(robotId,j)[1].decode("utf-8")))
+    print("%d - %s" % (p.getJointInfo(robotId, j)[0], p.getJointInfo(robotId, j)[1].decode("utf-8")))
 
 for i in range (10000):
 
     frictionForceBase = p.readUserDebugParameter(frictionIdBase)
     jointTorqueBase = p.readUserDebugParameter(torqueIdBase)
-    frictionForce = p.readUserDebugParameter(frictionId)
-    jointTorque = p.readUserDebugParameter(torqueId)
+    frictionForceTop = p.readUserDebugParameter(frictionIdTop)
+    jointTorqueTop = p.readUserDebugParameter(torqueIdTop)
 
     # setJointMotorControl2()
     # Permite configurar diferentes velocidades y fuerzas a las articulaciones.
@@ -60,7 +59,7 @@ for i in range (10000):
                             1, 
                             p.VELOCITY_CONTROL, 
                             targetVelocity=0, 
-                            force=frictionForce)
+                            force=frictionForceTop)
 
     # TORQUE_CONTROL
     # - Establece el torque o momento de fuerza instantáneamente sobre un joint.
@@ -73,7 +72,7 @@ for i in range (10000):
     p.setJointMotorControl2(robotId, 
                             1, 
                             p.TORQUE_CONTROL, 
-                            force=jointTorque)
+                            force=jointTorqueTop)
 
     p.stepSimulation()
     time.sleep(1./240.)

cf0847236f996483bc04a5c9f9787a6ab7d2cfbe
aleon2020 <a.leon.2020@alumnos.urjc.es>
2025-02-16 13:24:51 +0100

    UPLOADING FILES ...

--- Prácticas/Práctica 1: Dinámicas en Pybullet/src/Fase4_bullet.py
+++ Prácticas/Práctica 1: Dinámicas en Pybullet/src/Fase4_bullet.py
@@ -26,53 +26,59 @@ finishlineId = p.loadURDF("urdf/finish_line.urdf", [20, 0, 0], startOrientation)
 
 joints = [2, 3, 4, 5]
 
+# Set the realtime mode of the simulator
 p.setRealTimeSimulation(1)
 
-last_distance = -1
-start_time = time.time()
-csv_values = []
+last_advance = -1
+initial_time = time.time()
+data_records = []
 
 while (1):
 
     # p.stepSimulation()
     # time.sleep(1./240.)
 
-    # Escenario 3.1: Asignación velocidades y fuerzas
+    # Scenario 3.1: Assignment of speeds and forces
     speed = 15
     torque = 50
 
-    # Escenario 3.2: Asignación velocidades y fuerzas + fricción
+    # Scenario 3.2: Assignment of speeds and forces + friction
     # for i in joints:
     #     p.changeDynamics(huskyId, i, lateralFriction=0.93)
     #     p.changeDynamics(huskyId, i, spinningFriction=0.005)
     #     p.changeDynamics(huskyId, i, rollingFriction=0.003)
 
-    # Escenario 3.3: Asignación velocidades y fuerzas + fricción + Inercia
+    # Scenario 3.3: Assignment of speeds and forces + friction + Inertia
     # p.changeDynamics(barrierId, 0, localInertiaDiagonal=[20/3, 0.0, 20/3])
 
+    # Relates movement to the simultaneous activation of several joints
     p.setJointMotorControlArray(huskyId,
                                 joints,
                                 p.VELOCITY_CONTROL,
                                 targetVelocities=[speed, speed, speed, speed],
                                 forces=[torque, torque, torque, torque])
     
+    # Adjust the camera view in PyBullet to follow the Husky robot
     p.resetDebugVisualizerCamera(cameraDistance=5, 
                                  cameraYaw=p.getDebugVisualizerCamera()[8], 
                                  cameraPitch=p.getDebugVisualizerCamera()[9], 
                                  cameraTargetPosition=p.getBasePositionAndOrientation(huskyId)[0])
 
-    distance = p.getBasePositionAndOrientation(huskyId)[0][0]
-    if (distance != last_distance):
-        csv_values.append([time.time() - start_time, distance, p.getBaseVelocity(huskyId)[0][0], speed, torque])
-        last_distance = distance
+    # Collects and records information every 0.01 meters that the robot advances
+    current_advance = p.getBasePositionAndOrientation(huskyId)[0][0]
+    if (current_advance != last_advance):
+        data_records.append([time.time() - initial_time, current_advance, p.getBaseVelocity(huskyId)[0][0], speed, torque])
+        last_advance = current_advance
 
+    # The execution stops when the robot reaches the end (finish_line)
     if p.getBasePositionAndOrientation(huskyId)[0][0] >= 20:
         break
 
 p.disconnect()
 
+# All data is saved in a file in CSV format and exits PyBullet
 with open('Fase2.csv', mode = 'w', newline = '') as file:
     writer = csv.writer(file)
     writer.writerow(['tiempo', 'posicion_robot[Y]', 'velocidad_robot[Y]', 'velocidad_ruedas', 'fuerza_ruedas'])
-    for i in csv_values:
+    for i in data_records:
     	writer.writerow([i[0], i[1], i[2], i[3], i[4]])

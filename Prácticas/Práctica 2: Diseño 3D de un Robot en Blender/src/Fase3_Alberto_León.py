0a72bbe742f1e65b521a5c3e50d1984b649bee5f
aleon2020 <a.leon.2020@alumnos.urjc.es>
2025-03-27 10:53:35 +0100

    UPLOADING FILES ...

--- Prácticas/Práctica 2: Diseño 3D de un Robot en Blender/src/Fase3_Alberto_León.py
+++ Prácticas/Práctica 2: Diseño 3D de un Robot en Blender/src/Fase3_Alberto_León.py
@@ -19,18 +19,35 @@ numJoints = p.getNumJoints(robotId)
 print("NumJoints: " + str(numJoints))
 
 wheels_joints = [8, 10, 12, 14]
-gripper_left_joint = 5
-gripper_right_joint = 6
+gripper_joints = [5, 6]
 arm_gripper_joint = [4]
 
+gripper_movement = p.addUserDebugParameter("gripper_movement", 0, 5, 0)
+arm_gripper_movement = p.addUserDebugParameter("arm_gripper_movement", -25, 25, 0)
 
-p.changeDynamics(robotId, gripper_left_joint, jointLowerLimit=0, jointUpperLimit=-0.25)
-p.changeDynamics(robotId, gripper_right_joint, jointLowerLimit=0.25, jointUpperLimit=0.5)
+# p.changeDynamics(robotId, gripper_left_joint, jointLowerLimit=0, jointUpperLimit=-0.25)
+# p.changeDynamics(robotId, gripper_joints[0], jointLowerLimit=0.5, jointUpperLimit=0.75)
 
 for j in range (numJoints):
     print("%d - %s" % (p.getJointInfo(robotId,j)[0], p.getJointInfo(robotId,j)[1].decode("utf-8")))
     print("Link - %s" % (p.getJointInfo(robotId,j)[12]))
 
-input("Press enter to start motion")
-while True:
-    p.setJointMotorControlArray(robotId, wheels_joints, p.VELOCITY_CONTROL, targetVelocities=[11] * 4)
+# input("Press enter to start motion")
+while (1):
+
+    speed_gripper = p.readUserDebugParameter(gripper_movement)
+    speed_arm_gripper = p.readUserDebugParameter(arm_gripper_movement)
+    # p.setJointMotorControlArray(robotId, wheels_joints, p.VELOCITY_CONTROL, targetVelocities=[11] * 4)
+    
+    p.setJointMotorControlArray(robotId,
+                              gripper_joints,
+                              p.POSITION_CONTROL,
+                              targetVelocities=[speed_gripper, speed_gripper])
+
+    p.setJointMotorControlArray(robotId,
+                              arm_gripper_joint,
+                              p.POSITION_CONTROL,
+                              targetVelocities= [speed_arm_gripper])
+    
+    p.stepSimulation()
+    time.sleep(0.01)

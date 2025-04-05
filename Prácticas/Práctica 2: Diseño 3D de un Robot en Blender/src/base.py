bb0a94e54eac872c21378e9fb09fc76d02d566e5
aleon2020 <a.leon.2020@alumnos.urjc.es>
2025-04-05 11:09:04 +0200

    UPLOADING FILES ...

--- Prácticas/Práctica 2: Diseño 3D de un Robot en Blender/src/base.py
+++ Prácticas/Práctica 2: Diseño 3D de un Robot en Blender/src/base.py
@@ -1,23 +1,26 @@
+# Imports from libraries
 import pybullet as p
 import time
 import pybullet_data
 
+# We connect engine with GUI
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# We set gravity (X,Y,Z)
 p.setGravity(0,0, -9.8)
 
+# We load a model (plane)
 planeId = p.loadURDF("plane.urdf")
 
+# We load new objects, with a position (x,y,z)
+# and an orientation given in quaternion (C,X,Y,Z)
 startOrientation = p.getQuaternionFromEuler([0, 0, 3.14])
 robotId = p.loadURDF("robot_model/urdf/robot_model.urdf",[0, 0, 1.2], startOrientation)
-
 startOrientation = p.getQuaternionFromEuler([0, 0, 0])
 cubeId = p.loadURDF("robot_model/cube.urdf",[0, 4, 0], startOrientation)
 
-numJoints = p.getNumJoints(robotId)
-print("NumJoints: " + str(numJoints))
-
-wheels_joints = [8, 10, 12, 14]
+# Constants
 gripper_left_joint = 5
 gripper_right_joint = 6
 arm_gripper_joint = 4
@@ -26,10 +29,6 @@ p.changeDynamics(robotId, gripper_left_joint, jointLowerLimit=0, jointUpperLimit
 p.changeDynamics(robotId, gripper_right_joint, jointLowerLimit=0, jointUpperLimit=0.25)
 p.changeDynamics(robotId, arm_gripper_joint, jointLowerLimit=-1.25, jointUpperLimit=1)
 
-for j in range (numJoints):
-    print("%d - %s" % (p.getJointInfo(robotId,j)[0], p.getJointInfo(robotId,j)[1].decode("utf-8")))
-
 while (1):
-
     p.stepSimulation()
-    time.sleep(0.01)
+    time.sleep(1./240.)
